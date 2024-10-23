94c5af640ac148b17db4f571633bf47e90f03b09
duyqnet <duyqnet@gmail.com>
2024-10-23 09:59:04 +0800

    shop fragment

--- app/src/main/java/com/ignacio/partykneadsapp/menus/ShopFragment.java
+++ app/src/main/java/com/ignacio/partykneadsapp/menus/ShopFragment.java
@@ -1,5 +1,7 @@
 package com.ignacio.partykneadsapp.menus;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,28 +12,38 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 
 import com.ignacio.partykneadsapp.R;
+import com.ignacio.partykneadsapp.adapters.CategoriesAdapter;
 import com.ignacio.partykneadsapp.adapters.ProductShopAdapter;
+import com.ignacio.partykneadsapp.databinding.FragmentShopBinding;
+import com.ignacio.partykneadsapp.model.CategoriesModel;
 import com.ignacio.partykneadsapp.model.ProductShopModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class ShopFragment extends Fragment {
 
+    private LinearLayout cl;
     private List<ProductShopModel> productList; // Declare productList here
-
+    private RecyclerView categories;
+    private List<CategoriesModel> categoriesModelList;
+    private CategoriesAdapter categoriesAdapter;
+    FragmentShopBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shop, container, false);
-
+        binding = FragmentShopBinding.inflate(getLayoutInflater());
 //        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
 //        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = binding.recyclerView;
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
         productList = new ArrayList<>();
@@ -44,6 +56,45 @@ public class ShopFragment extends Fragment {
         ProductShopAdapter adapter = new ProductShopAdapter(getActivity(), productList);
         recyclerView.setAdapter(adapter);
 
-        return view;
+        setupCategories();
+
+        return binding.getRoot();
+
+
+
+    }
+
+
+    private void setupCategories() {
+        categories = binding.categories;
+        categoriesModelList = new ArrayList<>();
+        categoriesModelList.add(new CategoriesModel(R.drawable.cake, "Cakes"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.breads, "Breads"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.desserts, "Desserts"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.customized, "Customize"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.balloons, "Balloons"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.candles, "Candles"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.party_hats, "Party Hats"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.banners, "Banners"));
+        categoriesModelList.add(new CategoriesModel(R.drawable.confetti, "Confetti"));
+
+        categoriesAdapter = new CategoriesAdapter(getActivity(), categoriesModelList);
+        categories.setAdapter(categoriesAdapter);
+        categories.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        categories.setHasFixedSize(true);
+        categories.setNestedScrollingEnabled(false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        cl = view.findViewById(R.id.clayout);
+        cl.setOnClickListener(v -> {
+            InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        });
+
     }
 }
